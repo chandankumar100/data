ba494233cd5baaa157f7dfc8b890589b4dc5a4b6
chandankumar100 <118258402+chandankumar100@users.noreply.github.com>
2022-11-15 14:41:43 +0530

    Add files via upload

---
+++ alcohal_reader.ino
@@ -0,0 +1,24 @@
+  int gas =A1;
+   int led =4;
+  void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(gas,INPUT);
+pinMode(led,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(Serial.available()==0)
+int data= analogRead(gas);
+Serial.print(data);
+if(data == 23)
+{
+  digitalWrite(led,1);
+}
+else
+{
+  digitalWrite(led,0);
+}
+}
